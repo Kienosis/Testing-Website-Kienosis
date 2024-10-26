851e988d85d0c8e92cf48c1ec896a3d423e3c3d0
Kienosis <steventcull@gmail.com>
2024-10-25 20:08:11 -0400

    Web Updates

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function learnMore() {
     alert("Stay tuned for more updates on our hosting services!");
 }
+<script>
+    // Get all nav links
+    const navLinks = document.querySelectorAll("nav ul li a");
+    
+    // Loop through links to add active class to the current page
+    navLinks.forEach(link => {
+        if (link.href === window.location.href) {
+            link.classList.add("active");
+        }
+    });
+</script>
